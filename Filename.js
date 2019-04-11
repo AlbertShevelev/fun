9b5367f3e1ded7db5045c10bc7368e3f5065166c
AlbertShevelev <34644273+AlbertShevelev@users.noreply.github.com>
2019-04-11 23:12:36 +0900

    Add files via upload

---
+++ Filename.js
@@ -0,0 +1,15 @@
+document.body.background="black";
+var func=function (){
+	var div=document.createElement("div");
+	document.body.appendChild(div);
+	div.style.width=200+"px";
+	div.style.height=200+"px";
+	div.style.background="lightgreen";
+	div.style.position="absolute";
+	div.style.top=Math.round(Math.random()*innerHeight)-200+"px";
+	div.style.left=Math.round(Math.random()*innerWidth)-200+"px";
+	div.onclick=function (){
+		this.style.display="none";
+	}
+}
+setInterval(func,1000)
